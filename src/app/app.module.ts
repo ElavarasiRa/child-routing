4b12ee783c875c94b318a17442815eb56a5ba326
ElavarasiRa <relavarasi1996@gmail.com>
2019-05-24 11:09:50 +0530

    child routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentsComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'',pathMatch:'full',redirectTo:'header'},
+      {path:'header',component:HeaderComponent,
+    children:[
+      {path:'students',component:StudentsComponent},
+      {path:'contact',component:ContactComponent},
+      {path:'home',component: HomeComponent},
+    ]}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
